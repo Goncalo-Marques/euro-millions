e21191e68a54449d06afadf54481b89170f944dc
Goncalo Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2023-06-03 22:03:39 +0100

    feat: add config file

---
+++ configs.py
@@ -0,0 +1,85 @@
+# NUM_PLAYERS defines the number of concurrent players (clients)
+NUM_PLAYERS = 20
+
+# MAX_WAIT_TIME defines the maximum time, in seconds, to wait for new players
+MAX_WAIT_TIME = 5
+
+# MAX_CONCURRENT_BETS defines the maximum number of concurrent bets that can be performed
+MAX_CONCURRENT_BETS = 3
+
+
+# AMOUNT_GENERATED_NUMBERS defines the amount of numbers to generate
+AMOUNT_GENERATED_NUMBERS = 5
+# AMOUNT_GENERATED_STARS defines the amount of stars to generate
+AMOUNT_GENERATED_STARS = 2
+# AMOUNT_GENERATED_TOTAL defines the total amount of numbers being generated
+AMOUNT_GENERATED_TOTAL = AMOUNT_GENERATED_NUMBERS + AMOUNT_GENERATED_STARS
+
+# NUMBER_MIN defines the minimum possible number to be selected
+NUMBER_MIN = 1
+# NUMBER_MAX defines the maximum possible number to be selected
+NUMBER_MAX = 50
+
+# STAR_MIN defines the minimum possible star number to be selected
+STAR_MIN = 1
+# STAR_MAX defines the maximum possible star number to be selected
+STAR_MAX = 12
+
+
+# LOGS_FOLDER defines the path of the folder that contains the application logs
+LOGS_FOLDER = "./logs/"
+
+
+# PRIZE configurations
+PRIZE1_NUMBERS = 5
+PRIZE1_STARS = 2
+PRIZE1_AMOUNT = 17000000
+PRIZE1_AMOUNT_INCREMENT = 100000
+
+PRIZE2_NUMBERS = 5
+PRIZE2_STARS = 1
+PRIZE2_AMOUNT = 200738
+
+PRIZE3_NUMBERS = 5
+PRIZE3_STARS = 0
+PRIZE3_AMOUNT = 20851
+
+PRIZE4_NUMBERS = 4
+PRIZE4_STARS = 2
+PRIZE4_AMOUNT = 1299
+
+PRIZE5_NUMBERS = 4
+PRIZE5_STARS = 1
+PRIZE5_AMOUNT = 120
+
+PRIZE6_NUMBERS = 3
+PRIZE6_STARS = 2
+PRIZE6_AMOUNT = 57
+
+PRIZE7_NUMBERS = 4
+PRIZE7_STARS = 0
+PRIZE7_AMOUNT = 39
+
+PRIZE8_NUMBERS = 2
+PRIZE8_STARS = 2
+PRIZE8_AMOUNT = 14
+
+PRIZE9_NUMBERS = 3
+PRIZE9_STARS = 1
+PRIZE9_AMOUNT = 11
+
+PRIZE10_NUMBERS = 3
+PRIZE10_STARS = 0
+PRIZE10_AMOUNT = 9
+
+PRIZE11_NUMBERS = 1
+PRIZE11_STARS = 2
+PRIZE11_AMOUNT = 7
+
+PRIZE12_NUMBERS = 2
+PRIZE12_STARS = 1
+PRIZE12_AMOUNT = 6
+
+PRIZE13_NUMBERS = 2
+PRIZE13_STARS = 0
+PRIZE13_AMOUNT = 4
